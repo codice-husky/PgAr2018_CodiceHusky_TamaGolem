934b6a99e30ae16b61979e445625fe2f9a7cd869
Daniele Menotti <d.menotti@studenti.unibs.it>
2019-04-25 18:05:53 +0200

    Altri due metodini

--- src/codiceHusky/tamaGolem/ElementiArrayList.java
+++ src/codiceHusky/tamaGolem/ElementiArrayList.java
@@ -13,5 +13,20 @@ public class ElementiArrayList extends ArrayList<String> {
 		}
 		mappaPotenza = new Integer[TamaMain.NUMERO_ELEMENTI][TamaMain.NUMERO_ELEMENTI];
 	}
+	
+	
+	public void associaPotenza(String elementoSorgente, String elementoRicevente, int potenza) {
+		int indexSorgente = this.indexOf(elementoSorgente);
+		int indexRicevente = this.indexOf(elementoRicevente);
+		
+		mappaPotenza[indexSorgente][indexRicevente] = potenza;
+	}
+	
+	public int getPotenza(String elementoSorgente, String elementoRicevente) {
+		int indexSorgente = this.indexOf(elementoSorgente);
+		int indexRicevente = this.indexOf(elementoRicevente);
+		
+		return mappaPotenza[indexSorgente][indexRicevente];
+	}
 
 }
