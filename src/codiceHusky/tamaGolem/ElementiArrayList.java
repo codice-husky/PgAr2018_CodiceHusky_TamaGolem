c6faae2d9ee0048e23c6d77d86488d1dff6584d1
Daniele Menotti <d.menotti@studenti.unibs.it>
2019-04-25 17:54:40 +0200

    Miromodifiche

--- src/codiceHusky/tamaGolem/ElementiArrayList.java
+++ src/codiceHusky/tamaGolem/ElementiArrayList.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 
 public class ElementiArrayList extends ArrayList<String> {
 	private static final long serialVersionUID = 1L;
+	private Integer[][] mappaPotenza;
 	
 	public ElementiArrayList(String[] elementi) {
 		super();
 		for(String elemento : elementi) {
 			this.add(elemento);
 		}
+		mappaPotenza = new Integer[TamaMain.NUMERO_ELEMENTI][TamaMain.NUMERO_ELEMENTI];
 	}
 
 }
--- src/codiceHusky/tamaGolem/TamaMain.java
+++ src/codiceHusky/tamaGolem/TamaMain.java
@@ -1,5 +1,7 @@
 package codiceHusky.tamaGolem;
+
 public class TamaMain {
+	public static final int NUMERO_ELEMENTI = 4;
 	public static void main(String[] args) {
 		
 	}
