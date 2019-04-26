d36c83774de0bb5672b0b4673acfbec4a09a4791
Mattia Orioli <m.orioli002@studenti.unibs.it>
2019-04-26 16:56:01 +0200

    qualcosina in Giocatore

--- src/codiceHusky/tamaGolem/Giocatore.java
+++ src/codiceHusky/tamaGolem/Giocatore.java
@@ -3,6 +3,30 @@ package codiceHusky.tamaGolem;
 import java.util.ArrayList;
 
 public class Giocatore {
-	private ArrayList<TamaGolem> tamaGolem;
+	
+	private String nome;
+	private ArrayList<TamaGolem> squadra;
+	private TamaGolem golemAttivo;
+	
+	
+
+	public void aggiungiGolem(TamaGolem golem) {
+		squadra.add(golem);
+		
+	}
+	
+	
+	
+	public TamaGolem getGolemAttivo() {
+		return golemAttivo;
+	}
+
+
+	public void setGolemAttivo(TamaGolem golemAttivo) {
+		this.golemAttivo = golemAttivo;
+	}
+	
+	
+	
 	
 }
