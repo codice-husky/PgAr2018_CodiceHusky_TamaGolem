93c25856eabdc696cc66eda4dbca14ceee7be949
Daniele Menotti <d.menotti@studenti.unibs.it>
2019-04-27 17:33:48 +0200

    Ritocchini

--- src/codiceHusky/tamaGolem/TamaMain.java
+++ src/codiceHusky/tamaGolem/TamaMain.java
@@ -7,20 +7,30 @@ import org.apache.commons.lang3.StringUtils;
 
 public class TamaMain {
 	private static final int ELEM_MAX = 10;
-	private static final int ELEM_MIN = 1;
+	private static final int ELEM_MIN = 3;
 	public static final int VITA_TAMAGOLEM = 10;
 	public static final String[] ELEMENTI_PIETRE = {"Acqua", "Aria", "Fuoco", "Terra", "Torta", "Fagiolo", "Budino", "Pianta", "Sale", "Pepe"};
 	public static int elemUtilizzati;
+	
+	
 	public static final SaccaPietre pietre = new SaccaPietre();
+	private static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
+		System.out.println("---TAMAGOLEM CODICE HUSKY---");
+		do {
+			if(setup()) break;
+			System.out.println("Dato inserito non valido!");
+		} while(true);
+		
+		
+		
+		sc.close();
 	}
 	
 	public static boolean setup() {
-		Scanner in = new Scanner(System.in);
-		System.out.println("Quanti elementi vuoi utilizzare? (1-10, R)");
-		String inputStr = in.nextLine();
-		in.close();
+		System.out.print("Quanti elementi vuoi utilizzare? (" + ELEM_MIN + "-" + ELEM_MAX + ", [R]andom) ");
+		String inputStr = sc.nextLine();
 		if(StringUtils.isNumeric(inputStr)) {
 			int inputElem = Integer.parseInt(inputStr);
 			if(inputElem >= ELEM_MIN && inputElem <= ELEM_MAX) {
